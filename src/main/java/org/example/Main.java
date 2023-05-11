833be1777210cfc73d4e1493960148a4b6121eca
Umer Siddique <ummersiddique@gmail.com>
2023-05-11 10:23:04 +0500

    Method overloading

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        calculateSum(12, 23);
+        calculateSum(123, 23, 23, 23, 23, 23, 23, 23, 123);
+    }
+
+    // method signature ` calculateSum(int,int) `
+    public static int calculateSum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    // method signature ` calculateSum(int...) `
+    public static int calculateSum(int... numbers) {
+        int sum = 0;
+        if (numbers != null) {
+            for (int number : numbers) {
+                sum += number;
+            }
+        }
+        return sum;
     }
 }
